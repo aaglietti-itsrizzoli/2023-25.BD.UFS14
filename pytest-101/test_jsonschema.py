0451bd8cd7bcc77c2e1dc79bcd1541de1ae45069
aaglietti-itsrizzoli <104371332+aaglietti-itsrizzoli@users.noreply.github.com>
2024-09-12 11:06:11 +0000

    test: jsonschema

---
+++ pytest-101/test_jsonschema.py
@@ -0,0 +1,19 @@
+import json
+from pathlib import Path
+
+def func():
+    transformed_data = {
+        "greeting": "Welcome to quicktype!",
+        "instructions": [
+            "Type or paste JSON here",
+            "Or choose a sample above",
+            "quicktype will generate code in your",
+            "chosen language to parse the sample data"
+        ]
+    }
+    return json.dumps(transformed_data)
+
+
+def test_answer(schema_validate_string):
+    data_to_be_validated = func()
+    assert schema_validate_string(data=data_to_be_validated, schema_name="./test_jsonschema.contract.out", file_type="json")
