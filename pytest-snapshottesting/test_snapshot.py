246230b89da70922a1e3a6ee50030c4c41a9f83a
aaglietti-itsrizzoli <104371332+aaglietti-itsrizzoli@users.noreply.github.com>
2024-09-19 16:40:55 +0000

    test: snapshotesting 101

--- pytest-snapshottesting/test_snapshot.py
+++ pytest-snapshottesting/test_snapshot.py
@@ -1,6 +1,7 @@
 def func(x):
     return x + 1
 
-
-def test_answer():
-    assert func(3) == 4
+def test_answer(snapshot):
+    extract_ouput = func(23)
+    snapshot.snapshot_dir = 'snapshots'  # This line is optional.
+    snapshot.assert_match(str(extract_ouput), 'foo_output.txt')
