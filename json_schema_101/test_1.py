ef0c99f14281f6aca2fa2b6ca1892f4c0d457aab
ZaccagniDavide <davide.zaccagni@itsrizzoli.it>
2024-09-20 14:46:41 +0000

    ok

--- json_schema_101/test_1.py
+++ json_schema_101/test_1.py
@@ -1,5 +1,13 @@
 # content of test_sample.py
 from jsonschema import validate
+
+def func(x):
+    return x + 1
+
+
+def test_answer():
+    assert func(4) == 5
+
 def json_schema():
     schema = {
         "type" : "object",
@@ -21,4 +29,10 @@ def validate_wrapper(instance,schema):
         validate(instance=instance,schema=schema)
         return True
     except:
-        return False                                
+        return False  
+
+def test_function_output_with_snapshot(snapshot):
+    snapshot.snapshot_dir = 'snapshots'  # This line is optional.
+    pierino=func(5)
+    pierino_stringa=str(pierino)
+    snapshot.assert_match(pierino_stringa, 'foo_output.txt')                              
