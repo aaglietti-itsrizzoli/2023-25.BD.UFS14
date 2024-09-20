0b093b5173d26a679edbdb0d7778f18af53c6129
William_S <38282661+WillSant01@users.noreply.github.com>
2024-09-20 15:07:34 +0000

    chore: snapshots csv

--- json_schema _101/test_lez_3.py
+++ json_schema _101/test_lez_3.py
@@ -1,30 +1,52 @@
 from jsonschema import validate
 
-def json_schema():
-    schema = {
+schema = {
     "type" : "object",
     "properties" : {
         "price" : {"type" : "number"},
         "name" : {"type" : "string"},
     },
 }
-    
-    return schema
 
 
-def test_validate():
-    assert validate_wrapper(instance={"name" : "Eggs", "price" : 34.99}, schema=json_schema()) == True
-
-def test_validate_fail():
-    assert validate_wrapper(instance={"name" : "Eggs", "price" : "Ciao"}, schema=json_schema()) == False
+def func(x):
+    return x + 1
 
 
+def test_answer():
+    assert func(4) == 5
 
+
+def test_jschema_success():
+    validate(instance={"name" : "Eggs", "price" : 34.99}, schema=schema)
+
+def test_validation():
+    assert validate_wrapper(instance={"name" : "Eggs", "price" : 34.99}, schema=schema) == True
+
+def test_validation_fail():
+    assert validate_wrapper(instance={"name" : "Eggs", "price" : "Secondo me si "}, schema=schema) == False
+
+# creo un decoratore perchè la funzione con validate restituisce solo un 'none' e non un booleano T F che ci sarebbe molto più utile
 def validate_wrapper(instance, schema):
     try:
-        validate(instance = instance, schema = schema)
-        return True # None -> True
-    
+        validate(instance=instance, schema=schema)
+        return True # qui il none diventa true
     except:
-        return False # Errore -> False
-    
+        return False # qui l'errore che dava altrimenti diventa false
+
+
+#test usando snapshot
+def test_function_output_with_snapshot(snapshot):
+    snapshot.snapshot_dir = 'snapshots'
+    pierino = str(func(12))
+    snapshot.assert_match(pierino, 'foo_output.txt')
+
+frutti = """frutti,prezzo,colore,sapore
+pera,100,verde,buono
+mela,10,rosso,squisito
+ananas,23,giallo, croccante
+"""
+
+def test_function_output_with_snapshot_csv(snapshot):
+    snapshot.snapshot_dir = 'snapshots'
+    snapshot.assert_match(frutti, 'frutti.csv')
