c1de54df40ef60a1d08d8d50370f790790456f70
FenziFrancesc0 <francesco.fenzi@itsrizzoli.it>
2024-09-27 15:41:17 +0000

    add test_snapshot

---
+++ test_snapshot.py
@@ -0,0 +1,7 @@
+def coso():
+    return f'''due righe 
+            seconda riga'''
+            
+def test_function_output_with_snapshot(snapshot):
+    snapshot.snapshot_dir = 'snapshots'  # This line is optional.
+    snapshot.assert_match(coso(), 'coso_output.txt')
