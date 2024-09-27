8d4cdd18213ee19b558c35cb9e652a62cc60c926
MaddalenaB5 <maddalena.bozzola@itsrizzoli.it>
2024-09-27 13:55:30 +0000

    ci: snapshot

--- test_projectwork/test_file_func.py
+++ test_projectwork/test_file_func.py
@@ -1,11 +1,13 @@
 from file_func import controllo_validita_url
 from file_func import crea_dizionario
 
+#test di assert
 def test_controllo_validita_url():
     assert controllo_validita_url('https://google.com')== True
     assert controllo_validita_url('http://google.com')== True
     assert controllo_validita_url('https//google.com')== False 
 
+#test validita json
 def test_crea_dizionario():
     output = crea_dizionario([('a', 'b'), ('c', 'd'), ('e', 'f')])
     assert isinstance(output, dict) == True
@@ -13,3 +15,9 @@ def test_crea_dizionario():
         assert isinstance(chiave, str) == True
     for valore in output.values():
         assert isinstance(valore, str) == True
+
+#snapshot
+def test_snapshot(snapshot):
+    snapshot.snapshot_dir = 'snapshots_proj'
+    output_2 = str(crea_dizionario([('a', 'b'), ('c', 'd'), ('e', 'f')]))
+    snapshot.assert_match(output_2, 'proj_output.txt')
